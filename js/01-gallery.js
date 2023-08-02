a8322260afc4d798db8a4f55276b42db601c8cb1
kait-meshcheriakova <129512123+kait-meshcheriakova@users.noreply.github.com>
2023-08-03 00:58:28 +0300

    draft

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const container = document.querySelector(".gallery");
+console.log(container);
 
-console.log(galleryItems);
+
+function createMarkup( galleryItems){
+    
+    return  galleryItems.map((img ) => `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${img.original}">
+    <img
+      class="gallery__image"
+      src="${img.preview}"
+      data-source="${img.original}"
+      alt="${img.description}"
+    />
+  </a>
+  </li>`).join("")
+}
+container.insertAdjacentHTML('beforeend', createMarkup( galleryItems));
+
+container.addEventListener("click ", handlerImageClick);
+let instance; 
+function handlerImageClick (event){
+  event.preventDefault()
+ 
+    if (!event.target.classList.contains("gallery__image")){
+       return}
+        instance = basicLightbox.create(`
+       <img src="${target.getAttribute('data-source')}">
+     `, {
+           onShow() {
+               document.addEventListener('keydown', closeModal);
+           },
+           onClose() {
+               document.removeEventListener('keydown', closeModal);
+           },
+       });
+       instance.show();
+   }
+   
+   
+   function closeModal(event) {
+       if (event.code === 'Escape') {
+           instance.close()
+       }
+   }
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const container = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function createMarkup(galleryItems){
+    return  galleryItems.map((img ) => `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${img.original}">
+    <img
+      class="gallery__image"
+      src="${img.preview}"
+      alt="${img.description}"
+    />
+  </a>
+  </li>`).join("")
+}
+// const markup = galleryItems.map(image => {
+//     return `<li class = "gallery__item" ><a class="gallery__link" href="${image.original}"><img src="${image.preview}"  class="gallery__image" alt ="${image.description}" /></a></li>`
+// }).join('');
+ container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+// container.insertAdjacentHTML('beforeend', markup)
+container.addEventListener("click ", handlerImageClick);
+// let lightbox= new SimpleLightbox('.gallery li a', { captionType: 'attr', captionsData: 'alt', captionDelay: 250 });
+function handlerImageClick(event) {
+    event.preventDefault()
+    if (!event.currenTarget.classList.contains("gallery__image")) {
+         return
+    }
+    
+ }
+ let lightbox= new SimpleLightbox('.gallery li a', { captionType: 'attr', captionsData: 'alt', captionDelay: 250 });
